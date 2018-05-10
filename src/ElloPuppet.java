cd80f9ce0d21ba53dddcc1a6cfa844f486ef8ee6
yannage <yannissl4@gmail.com>
2018-05-10 17:17:01 -0400

    Added a 3rd line of code
    Added in "It's the puppet master!"

--- src/ElloPuppet.java
+++ src/ElloPuppet.java
@@ -4,6 +4,7 @@ public class ElloPuppet {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ello Puppet");	
+		System.out.println("It's the puppet master!");	
 	}
 
 }
